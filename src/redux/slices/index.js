3ddf6469d62945357eba0be62fe6799c8cd6f02d
ArkarOo <akoucsmub@gmail.com>
2024-07-27 20:58:03 +0630

    feat: modify

--- src/redux/slices/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import CustomThemeSlice from "./CustomThemeSlice";
-
-const rootReducer = combineReducers({
-  theme: CustomThemeSlice,
-  // Add other slices here
-});
-
-export default rootReducer;
--- src/styles/CommonStyles.js
+++
@@ -1,8 +0,0 @@
-const aTalkBorderRadius = {
-  borderTopLeftRadius: 16,
-  borderTopRightRadius: 0,
-  borderBottomLeftRadius: 0,
-  borderBottomRightRadius: 16,
-};
-
-export default { aTalkBorderRadius };
